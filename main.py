83f0dd65b2c16df7e4e7c6839b7c1381fbbbf513
Dericktional <nguyenderick5+GitHub@gmail.com>
2025-09-05 22:40:22 -0700

    CH3L2 Arguments Completed

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import sys
 from stats import *
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        book = get_book_text(f)
+    if len(sys.argv) != 1:
+        with open(sys.argv[1]) as f:
+            book = get_book_text(f)
 
-        print(
-            f"============ BOOKBOT ============\n"
-            f"Analyzing book found at books/frankenstein.txt...\n"
-            f"----------- Word Count ----------\n"
-            f"Found {get_num_words(book)} total words\n"
-            f"--------- Character Count -------\n"
-        )
+            print(
+                f"============ BOOKBOT ============\n"
+                f"Analyzing book found at {sys.argv[1]}...\n"
+                f"----------- Word Count ----------\n"
+                f"Found {get_num_words(book)} total words\n"
+                f"--------- Character Count -------\n"
+            )
 
-        for dict in sort_char_count(get_char_count(book)):
-            if dict["char"].isalpha():
-                print(f"{dict["char"]}: {dict["count"]}")
+            for dict in sort_char_count(get_char_count(book)):
+                if dict["char"].isalpha():
+                    print(f"{dict["char"]}: {dict["count"]}")
 
+    else:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
